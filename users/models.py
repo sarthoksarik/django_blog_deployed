1bc948b29bb4996e513aa5a213e4a25bceacb259
sarthoksarik <kirasnamdas@gmail.com>
2020-05-25 02:11:01 +0600

    image problem fixed

--- users/models.py
+++ users/models.py
@@ -16,6 +16,25 @@ class Profile(models.Model):
         super().save(*args, **kwargs)
 
         img = Image.open(self.image.path)
+        try:
+
+            for orientation in ExifTags.TAGS.keys():
+                if ExifTags.TAGS[orientation] == 'Orientation':
+                    break
+            exif = dict(img._getexif().items())
+
+            if exif[orientation] == 3:
+                img = img.rotate(180, expand=True)
+            elif exif[orientation] == 6:
+                img = img.rotate(270, expand=True)
+            elif exif[orientation] == 8:
+                img = img.rotate(90, expand=True)
+
+            img.thumbnail((img.width, img.height), img.ANTIALIAS)
+            img.save(self.image.path)
+
+        except:
+            pass
 
         if img.height > 300 or img.width > 300:
             output_size = (300, 300)
