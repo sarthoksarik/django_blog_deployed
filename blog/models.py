8db2275310e4949b34485bb575467542fb1f5a2e
sarthoksarik <kirasnamdas@gmail.com>
2020-06-15 16:21:44 +0600

    Like count implemented in models.py Like button is to be implemented Template shows like count and like button, but like button is currently not activated

--- blog/models.py
+++ blog/models.py
@@ -10,11 +10,18 @@ class Post(models.Model):
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
+    # upvotes will be similar to 'like' in facebook and toggleable
+    upvotes = models.ManyToManyField(
+        User, related_name='upvoter', blank=True)
+
     def __str__(self):
         return self.title
 
     def get_absolute_url(self):
         return reverse('post-detail', kwargs={'pk': self.pk})
+
+    # def get_total_likes(self):
+    #     return self.upvotes.count()
 
 
 class Announcements(models.Model):
